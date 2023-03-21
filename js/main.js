a9291836c81a0e68648d3ace599c0346c7572823
sukhrobbeksoatov <suhrobbek_soatov@mail.ru>
2023-03-21 21:25:05 +0500

    add modal to project

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 document.addEventListener("click", (evt) => {
   navToggleClick(evt);
+  modalOpenClick(evt);
+  modalCloseClick(evt);
+  modalOutsideCloseClick(evt);
 });
 
 function navToggleClick(evt) {
@@ -11,4 +14,33 @@ function navToggleClick(evt) {
   const elSelector = el.dataset.navToggler;
 
   document.querySelector(`[${elSelector}]`).classList.toggle("show");
+}
+
+function modalOpenClick(evt) {
+  const el = evt.target.closest("[data-modal-open]");
+
+  if (!el) return;
+
+  const elSelector = el.dataset.modalOpen;
+
+  document.querySelector(`${elSelector}`).classList.add("show");
+  document.body.classList.add("blocked");
+}
+
+function modalCloseClick(evt) {
+  const el = evt.target.closest("[data-modal-close]");
+
+  if (!el) return;
+
+  el.parentElement.parentElement.parentElement.classList.remove("show");
+  document.body.classList.remove("blocked");
+}
+
+function modalOutsideCloseClick(evt) {
+  const el = evt.target;
+
+  if (!el.matches("[data-modal]")) return;
+
+  document.body.classList.remove("blocked");
+  el.classList.remove("show");
 }
